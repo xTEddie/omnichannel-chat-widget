00c5d7e7078b6d69fffe953e6accc7919ea62f76
Edward Tran <edtran@microsoft.com>
2023-09-12 04:11:04 -0700

    Add alternative way to create encoded data to bypass threshold

--- chat-widget/src/components/devicetransferqrpanestateful/DeviceTransferQrPaneStateful.tsx
+++ chat-widget/src/components/devicetransferqrpanestateful/DeviceTransferQrPaneStateful.tsx
@@ -5,6 +5,7 @@ import useChatContextStore from "../../hooks/useChatContextStore";
 import { ILiveChatWidgetContext } from "../../contexts/common/ILiveChatWidgetContext";
 import { ILiveChatWidgetAction } from "../../contexts/common/ILiveChatWidgetAction";
 import { LiveChatWidgetActionType } from "../../contexts/common/LiveChatWidgetActionType";
+import LiveChatContext from "@microsoft/omnichannel-chat-sdk/lib/core/LiveChatContext";
 
 export const DeviceTransferQrPaneStateful = () => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -15,6 +16,26 @@ export const DeviceTransferQrPaneStateful = () => {
 
     useEffect(() => {
         console.log("[DeviceTransferQrPaneStateful]");
+        const encodeData = (data: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
+            const stringifiedData = JSON.stringify(data);
+            const encodedData = (window as any).btoa(stringifiedData); // eslint-disable-line @typescript-eslint/no-explicit-any
+            return encodedData;
+        };
+
+        const createAlternativeData = (liveChatContext: LiveChatContext) => {
+            const alternativeData: any = {}; // eslint-disable-line @typescript-eslint/no-explicit-any
+            alternativeData.requestId = (liveChatContext as LiveChatContext).requestId;
+            alternativeData.chatId = (liveChatContext as LiveChatContext).chatToken.chatId;
+            alternativeData.reconnectId = (liveChatContext as LiveChatContext).reconnectId || undefined;
+            return alternativeData;
+        };
+
+        const createQrUrl = (data: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
+            const originURL = window.location.href;
+            const qrURL = `${originURL}?livechatcontext=${data}`;
+            return qrURL;
+        };
+
         const readBlobAsDataURL = (blob: Blob) => {
             return new Promise((resolve) => {
                 const reader = new FileReader();
@@ -25,19 +46,29 @@ export const DeviceTransferQrPaneStateful = () => {
             });
         };
 
-        const init = async () => {
-            const liveChatContext = await chatSDK?.getCurrentLiveChatContext();
-            const data = JSON.stringify(liveChatContext);
-            const encodedData = (window as any).btoa(data); // eslint-disable-line @typescript-eslint/no-explicit-any
-            const originURL = window.location.href;
-            const qrURL = `${originURL}?livechatcontext=${encodedData}`;
-            console.log(qrURL);
-
-            const qrApiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrURL}`;
+        const retrieveQrCode = async (qrData: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
+            const qrApiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrData}`;
             const response = await fetch(qrApiUrl);
             const blobResponse = await response.blob();
             const blobUrl = await readBlobAsDataURL(blobResponse);
+            return blobUrl;
+        };
+
+        const init = async () => {
+            const liveChatContext = await chatSDK?.getCurrentLiveChatContext();
+            let encodedData = encodeData(liveChatContext);
+
+            const encodedDataThreshold = 2500; // QRCode fails to be generated when chars length > ~3k
+            if (encodedData.length > encodedDataThreshold) {
+                encodedData = createAlternativeData(liveChatContext as LiveChatContext);
+            }
+
+            const qrURL = createQrUrl(encodedData);
+            console.log(qrURL);
+
+            const blobUrl = await retrieveQrCode(qrURL);
             console.log(blobUrl);
+
             setDeviceTransferQr(blobUrl);
         };
 
